b97a2d928a0848be667965d90c28476984a1fd2e
NelsonBlakeN <nelson.blaken@gmail.com>
2018-04-12 12:52:27 -0500

    Add second label in a box

--- today_page.py
+++ today_page.py
@@ -17,17 +17,24 @@ class Clock(Gtk.Window):
         self.set_size_request(1920,1080)
         self.set_title("TODAY")
         self.connect("destroy",Gtk.main_quit)
+        
+        main_box = Gtk.Box.new(Gtk.Orientation.HORIZONTAL, 40)
 
-        self.label = Gtk.Label()
-        self.label.set_xalign(1)
-        self.label.set_yalign(0)
+        self.label1 = Gtk.Label()
+        # self.label1.set_xalign(1)
+        # self.label1.set_yalign(0)
+        
+        self.label2 = Gtk.Label("Label 2")
+        
+        main_box.pack_start(self.label1, True, True, 0)
+        main_box.pack_end(self.label2, True, True, 0)
 
-        self.add(self.label)
+        self.add(main_box)
         self.show_all()
 
 
     def update(self): 
-        self.label.set_markup(time.strftime('<span foreground="white" font="28.5">%H:%M:%S</span>'))
+        self.label1.set_markup(time.strftime('<span foreground="white" font="28.5">%H:%M:%S</span>'))
         return True
 
 
